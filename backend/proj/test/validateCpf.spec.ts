024121cbac82906e8ca501066d18ef6bad96a455
Douglas Poma <douglaspoma@yahoo.com>
2024-03-27 07:28:28 -0300

    feat: refactor cpf

---
+++ backend/proj/test/validateCpf.spec.ts
@@ -0,0 +1,34 @@
+import { validateCpf } from '../src/validateCpf'
+
+test.each(['987.654.321-00', '974.563.215-58', '714.287.938-60'])(
+  'Deve testar se o cpf %s é válido',
+  (cpf: string) => {
+    const isCPFValid = validateCpf(cpf)
+    expect(isCPFValid).toBe(true)
+  },
+)
+test.each([
+  '000.000.000-00',
+  '111.111.111-11',
+  '222.222.222-22',
+  '333.333.333-33',
+  '444.444.444-44',
+  '555.555.555-55',
+  '666.666.666-66',
+  '777.777.777-77',
+  '888.888.888-88',
+  '999.999.999-99',
+])('Deve testar o cpf %s inválido com mesmo dígito', (cpf: string) => {
+  const isCPFValid = validateCpf(cpf)
+  expect(isCPFValid).toBe(false)
+})
+test.each([
+  '974.563.215-5',
+  '974.563.215-581234',
+  '974.563.215-AB',
+  null,
+  undefined,
+])('Deve testar cpf %s inválido', (cpf: any) => {
+  const isCPFValid = validateCpf(cpf)
+  expect(isCPFValid).toBe(false)
+})
